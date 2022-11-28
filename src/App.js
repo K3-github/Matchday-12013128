36bef1936094e2c798ae11f37f33021c0f90bbde
Krishnveer Bhagod <kbhagod@gmail.com>
2022-11-28 17:18:33 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Home from './Home';
+import {BrowserRouter as Router, Routes, Route}from "react-router-dom";
 import Video from './Video.js';
 
 function App() {
   return (
     <div className="app">
-      <Home/>
-      {/* <Video/> */}
+       <Router>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/match" element={<Video/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
